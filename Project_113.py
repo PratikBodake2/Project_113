d910718b1a087f49dc6cef5571880607734adaa6
PratikBodake2 <138816977+PratikBodake2@users.noreply.github.com>
2023-07-17 00:21:46 +0530

    Add files via upload

---
+++ Project_113.py
@@ -0,0 +1,61 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "D:/pythonprojects/project_113"
+to_dir ="D:/pythonprojects/class_113"
+
+# Event Hanlder Class
+class FileEventHandler(FileSystemEventHandler):
+
+    #1_on_created
+    def on_created(self, event):
+        print("hey , {event.src_path} has been created")
+    #2_on_deleted
+    def on_deleted(self, event):
+        print("Oops! , {event.src_path} has been deleted")
+    #3_on_modified
+    def on_modified(self, event):
+        print("hey there! , {event.src_path} has been modified")
+    #4_on_moved
+    def on_created(self, event):
+        print("Someone moved  {event.src_path} to {event.dest_path}")
+        
+
+
+# Initialize Event Handler Class
+event_handler = FileEventHandler()
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+
+#5_Write a exception for keyboardInterrupt
+
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+
+except KeyboardInterrupt:
+    print("Stopped! ")
+    observer.stop()
+
+
+
+
+
